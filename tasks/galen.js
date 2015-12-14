79d6a1dfc8e471891930639cff6754ab971f2c20
Martin Reinhardt <contact@martinreinhardt-online.de>
2015-12-14 22:06:55 +0100

    Use galenframework node wrapper

--- tasks/galen.js
+++ tasks/galen.js
@@ -1,7 +1,7 @@
 /*
  * @name grunt-galen
  * @author mjurczyk
- * 
+ *
  * @exports task.galen
  */
 
@@ -33,9 +33,9 @@ module.exports = function (grunt) {
     var options = this.options() || {};
     var done = this.async();
     var files = this.filesSrc;
-    
+
     var filesTmpDir = '.tmp_grunt-galen';
-    
+
     var galenCliAvailable;
 
     /*
@@ -43,50 +43,50 @@ module.exports = function (grunt) {
      * @private
      */
     var reports = [];
-    
+
     /**
      * Check if galen commandline tool is available. If not, substitute
      * command will be used.
-     * 
+     *
      * Also, if commandline tool is not present, inform the user that
      * it might be necessary to download galen, which can take some time.
-     * 
+     *
      * @param {Function} callback function callback
      */
-    function checkGalenCli (callback) {
+    function checkGalenCli(callback) {
       childprocess.exec('galen -v', function (err, output, erroutput) {
         if (err) {
           galenCliAvailable = false;
-          
-          if(!grunt.file.exists(__dirname + '/../galen-cli/lib/galen') ||
-             !grunt.file.exists(__dirname + '/../galen-cli/lib/galen.bat')) {
+
+          if (!grunt.file.exists(__dirname + '/../node_modules/galenframework/galen') || !grunt.file.exists(__dirname + '/../node_modules/galenframework/galen.cmd')) {
             log('Galen framework was not found. It will be downloaded during the first test.'.yellow);
             log('Please be patient as the download can take a moment.'.yellow);
           }
         } else {
-          galenCliAvailable = true; 
+          galenCliAvailable = true;
         }
-        
+
         callback();
       });
     }
-    
+
     /**
      * Determine whether gl.js should be used during the build.
      * Unless options.nogl is set to true, that is a case.
-     * 
+     *
      * If necessary, duplicate the `local` copy of gl.js from ./lib/gl.js
      * and put it in the cwd directory.
-     * 
+     *
      * @param {Function} callback function callback
      */
-    function checkLibrary (callback) {
+    function checkLibrary(callback) {
       var glPath = (options.cwd || '.') + '/gl.js';
-      
+
       if (typeof callback !== 'function') {
-        callback = function () {};
+        callback = function () {
+        };
       }
-      
+
       if (options.nogl === true) {
         callback();
       } else {
@@ -109,14 +109,14 @@ module.exports = function (grunt) {
      * to JSON conversion directly in Java. To avoid that, config file
      * is generated and imported in gl.js (it can also be directly
      * imported in test files, if proper config module is created).
-     * 
+     *
      * Config file contains all necessary information from the task
      * config, and uses config#set method from gl.js to put it into
      * test suites.
-     * 
+     *
      * @param {Function} callback function callback
      */
-    function buildConfigFile (callback) {
+    function buildConfigFile(callback) {
       var data = {};
 
       if (options.project) {
@@ -125,13 +125,13 @@ module.exports = function (grunt) {
 
       data.url = options.url || '';
       data.devices = options.devices || {};
-      
+
       Object.keys(data.devices).forEach(function (deviceName) {
         var device = data.devices[deviceName];
-        
+
         Object.keys(device.desiredCapabilities || {}).forEach(function (param) {
           if (typeof device.desiredCapabilities[param] !== 'string') {
-            grunt.fail.fatal('All desiredCapabilities have to be string variables. [failed on capability `' + param + '`]');      
+            grunt.fail.fatal('All desiredCapabilities have to be string variables. [failed on capability `' + param + '`]');
           }
         });
       });
@@ -140,12 +140,12 @@ module.exports = function (grunt) {
         data.seleniumGrid = {};
 
         data.seleniumGrid.url = options.seleniumGrid.url || [
-          'http://',
-          options.seleniumGrid.username,
-          ':',
-          options.seleniumGrid.accessKey,
-          '@ondemand.saucelabs.com:80/wd/hub'
-        ].join('');
+            'http://',
+            options.seleniumGrid.username,
+            ':',
+            options.seleniumGrid.accessKey,
+            '@ondemand.saucelabs.com:80/wd/hub'
+          ].join('');
       }
 
       grunt.file.write((options.cwd || '.') + '/gl.config.js', [
@@ -153,55 +153,55 @@ module.exports = function (grunt) {
         JSON.stringify(data),
         ');\n'
       ].join(''));
-      
+
       callback();
     }
-    
+
     /**
      * Concatenate all test files into a single file to speed
      * up the testing process.
-     * 
+     *
      * File is created in current working directory, under /.tmp_grunt-galen/ subdir.
-     * 
+     *
      * @param {Function} callback function callback
      */
-    function buildConcatFile (callback) {
+    function buildConcatFile(callback) {
       var testFiles = getTestingFiles();
       var concat = ['load(\'../gl.js\');'];
       var tmpFile = (options.cwd || '.') + '/' + filesTmpDir + '/galenTestConcated_' + (new Date()).getTime() + '.test.js';
-      
+
       if (testFiles.length > 1 && options.concat === true) {
         testFiles.forEach(function (file) {
           var fileSrc = grunt.file.read(file);
-          
+
           concat.push([
             '(function () {',
             fileSrc.replace(/^load\(('|"){1}.*\/?gl.js('|"){1}\);?$/gm, ''),
             '})();'
           ].join('\n\r'));
         });
-        
+
         grunt.file.write(tmpFile, concat.join('\n\r'));
-        
+
         files = [tmpFile];
       }
-      
+
       callback();
     }
-    
+
     /**
      * Remove temporary concat file.
-     * 
+     *
      * @see buildConcatFile
      * @param {Function} callback function callback
      */
-    function removeConcatFile (callback) {
+    function removeConcatFile(callback) {
       var tmpDir = (options.cwd || '.') + '/' + filesTmpDir;
-      
+
       if (grunt.file.exists(tmpDir)) {
         grunt.file.delete(tmpDir);
       }
-      
+
       callback();
     }
 
@@ -210,35 +210,35 @@ module.exports = function (grunt) {
      * @param   {String}  file path
      * @returns {Boolean} file existentional feelings
      */
-    function fileExists (file) {
+    function fileExists(file) {
       return grunt.file.exists(file);
     }
-    
+
     /**
      * Get all test files. Validate if file exists.
-     * 
+     *
      * @returns {Array} array of files
      */
-    function getTestingFiles () {
+    function getTestingFiles() {
       var testFiles = [];
 
       files.filter(fileExists)
-      .forEach(function (filePath) {
-        testFiles.push(filePath);
-      });
-      
+        .forEach(function (filePath) {
+          testFiles.push(filePath);
+        });
+
       return testFiles;
     }
 
     /**
      * Start the testing process. Generate shell terminal commands
      * and spawn them as separate child processes.
-     * 
+     *
      * When all processes finish, terminate the task.
-     * 
+     *
      * TODO: rename `cb` to `callback`s.
      */
-    function runGalenTests (cb) {
+    function runGalenTests(cb) {
       var testFiles = getTestingFiles();
       var htmlReport = options.htmlReport === true ? '--htmlreport ' + (options.htmlReportDest || '') : '';
       var testngReport = options.testngReport === true ? '--testngreport ' + (options.testngReportDest || '') : '';
@@ -248,7 +248,7 @@ module.exports = function (grunt) {
       testFiles.forEach(function (filePath) {
         resultPadding = Math.max(resultPadding, filePath.length);
       });
-      
+
       if (!options.seleniumGrid) {
         log('Starting', 'local'.green, 'Galen');
       } else {
@@ -283,7 +283,7 @@ module.exports = function (grunt) {
                 /*
                  * This line can be uncommented to show warnings in the console.
                  */
-                log(' (! ' + erroutput.yellow  + ') ');
+                log(' (! ' + erroutput.yellow + ') ');
               } else {
                 log('failed'.red);
                 reports.push(erroutput);
@@ -316,7 +316,7 @@ module.exports = function (grunt) {
      */
     function isFailed(testLog) {
       var failed = /Total failures: (.*)\n/g.exec(testLog);
-      failed = parseInt(failed.toString().replace('Total failures: ',''));
+      failed = parseInt(failed.toString().replace('Total failures: ', ''));
       return failed != 0;
     }
 
@@ -324,14 +324,14 @@ module.exports = function (grunt) {
      * Generate reports, print them if necessary, and finish
      * the async Grunt task with done().
      */
-    function finishGalenTests (cb) {
+    function finishGalenTests(cb) {
       var testLog = reports.join('\n\r');
 
       var total = /Total tests: (.*)\n/g.exec(testLog);
-      total = parseInt(total.toString().replace('Total tests: ',''));
+      total = parseInt(total.toString().replace('Total tests: ', ''));
 
       var failed = /Total failures: (.*)\n/g.exec(testLog);
-      failed = parseInt(failed.toString().replace('Total failures: ',''));
+      failed = parseInt(failed.toString().replace('Total failures: ', ''));
 
       var passed = total - failed;
 
@@ -348,7 +348,7 @@ module.exports = function (grunt) {
         log(testLog);
       }
 
-      log(status.passed + ' tests passed [' + parseInt(status.percentage) + '%]' );
+      log(status.passed + ' tests passed [' + parseInt(status.percentage) + '%]');
 
       if (status.failed) {
         grunt.fail.warn(status.failed + ' test failed [' + parseInt(100 - status.percentage) + '%]');
@@ -375,8 +375,8 @@ module.exports = function (grunt) {
 
       done();
     });
-    
-    process.on('uncaughtException', function(err) {
+
+    process.on('uncaughtException', function (err) {
       console.error(err.stack);
       grunt.fail.fatal(err);
     });
